8daa4ac64edaf47bdfa6b672ef85394bddb5687d
Tor Røkkum Børset <torrokku@gmail.com>
2018-01-23 00:11:52 +0100

    Startet en ny challenge

---
+++ js/monsterapp.js
@@ -0,0 +1,26 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false
+  },
+  methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function() {
+    },
+    specialAttack: function(){
+
+    },
+    heal: function () {
+
+    },
+    giveUp: function () {
+
+    }
+  }
+})
