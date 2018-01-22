4c29558e2ee2fffb8ce8ddf676a9fc979849b1bc
Tor Røkkum Børset <torrokku@gmail.com>
2018-01-23 00:46:14 +0100

    Lagt til funksjonalitet i monster spill

--- js/monsterapp.js
+++ js/monsterapp.js
@@ -11,9 +11,18 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
-    attack: function() {
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+
+      if(this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(5, 12);
+
+      this.checkWin();
     },
-    specialAttack: function(){
+    specialAttack: function () {
 
     },
     heal: function () {
@@ -21,6 +30,28 @@ new Vue({
     },
     giveUp: function () {
 
-    }
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You won! New game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('You lost! New game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+    
   }
 })
