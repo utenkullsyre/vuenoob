3102169b7a698f66afe18dc0a8088a06ec1f1312
Børset Tor Røkkum <TOBORS@vegvesen.no>
2018-01-23 09:55:29 +0100

    Ferdig monsterapp

--- js/monsterapp.js
+++ js/monsterapp.js
@@ -3,33 +3,63 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   methods: {
     startGame: function () {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function () {
-      this.monsterHealth -= this.calculateDamage(3, 10);
-
-      if(this.checkWin()) {
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits monster for ' + damage + ' hp'
+      });
+      if (this.checkWin()) {
         return;
       }
-
-      this.playerHealth -= this.calculateDamage(5, 12);
-
-      this.checkWin();
+      this.monsterAttacks();
     },
     specialAttack: function () {
-
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits monster hard for ' + damage + ' hp'
+      });
+      if (this.checkWin()) {
+        return;
+      };
+      this.monsterAttacks();
     },
     heal: function () {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      }
+      this.turns.unshift({
+        isPlayer:true,
+        text: 'Player heals for 10 hp'
+      })
+      this.monsterAttacks();
 
     },
     giveUp: function () {
-
+      this.turns = [];
+      this.gameIsRunning = false;
+    },
+    monsterAttacks: function () {
+      var damage =  this.calculateDamage(5, 12);
+      this.playerHealth -= damage
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits player for ' + damage + 'hp'
+      });
+      this.checkWin();
     },
     calculateDamage: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -39,6 +69,7 @@ new Vue({
         if (confirm('You won! New game?')) {
           this.startGame();
         } else {
+          this.turns.length = 0
           this.gameIsRunning = false;
         }
         return true;
@@ -52,6 +83,6 @@ new Vue({
       }
       return false;
     },
-    
+
   }
 })
